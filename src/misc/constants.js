b44a34f3752f16f4388c7c22396090a4fea0ab03
Inetol <admin@inetol.net>
2021-12-04 13:45:47 +0100

    constants update

--- src/misc/constants.js
+++ src/misc/constants.js
@@ -1,21 +1,24 @@
 'use strict';
 
 export let constants
+export let info
+
 constants = {
     baseURL: 'https://jspaste.tk',
-    routeURL: '/documents/',
-    info: {
-        name: 'JSPaste',
-        version: '9.0.0', // import('../../package.json').version // --experimental-json-modules
-        creator: 'tnfAngel#6557',
-        web: 'https://jspaste.tk',
-        github: 'https://github.com/tnfAngel/jspaste-api',
-        credits: [{
-            creator: 'Inetol#0840',
-            github: 'https://github.com/InetolVault/rdtspaste'
-        }, {
-            creator: 'Aidak',
-            github: 'https://github.com/Aidakkk/gopaste'
-        }]
-    }
+    routeURL: '/documents/'
+}
+
+info = {
+    name: 'JSPaste',
+    version: '9.0.0', // import('../../package.json').version // --experimental-json-modules
+    creator: 'tnfAngel#6557',
+    web: 'https://jspaste.tk',
+    github: 'https://github.com/tnfAngel/jspaste-api',
+    credits: [{
+        creator: 'Inetol#0840',
+        github: 'https://github.com/Inetol/jspaste-api'
+    }, {
+        creator: 'Aidak',
+        github: 'https://github.com/Aidakkk/gopaste'
+    }]
 }

37b3a389b660a31f003518b99baeab1252ec1ffa
Inetol <admin@inetol.net>
2023-09-25 19:30:00 +0200

    fix extenal URL import
    Signed-off-by: Inetol <admin@inetol.net>

--- lib/core/JSP/Access.ts
+++ lib/core/JSP/Access.ts
@@ -1,6 +1,5 @@
 import Request from "../Request.ts";
 import {api} from "../../static/api/v1.ts";
-import {URL} from "url";
 
 export default async function access(resource: string): Promise<IAccessRes> {
     const response = await new Request("GET", api.route.documents).access(resource);
@@ -11,7 +10,6 @@ export default async function access(resource: string): Promise<IAccessRes> {
             resource: resource
         },
         res: {
-            // TODO: Breaking change: string to URL
             url: new URL(api.url + resource),
             raw: response.raw,
             payload: response.api.data
